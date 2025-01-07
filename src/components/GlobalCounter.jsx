8d64107448927838339f3f22096051a7c1493006
Vimal  Jain <vimaljain@Vimals-MacBook-Air.local>
2025-01-07 16:13:52 +0530

    chnage counter

--- src/components/GlobalCounter.jsx
+++ src/components/GlobalCounter.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { COUNTER_DATA } from "../utils/helper";
 
-const GlobalCounter = () => {
+const BusinessCounter = () => {
     const AnimateCount = ({ start, end }) => {
         const [value, setValue] = useState(start);
 
         useEffect(() => {
             let startTimestamp = null;
             const duration = 2000;
-
             const step = (timestamp) => {
                 if (!startTimestamp) startTimestamp = timestamp;
                 const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-                setValue((progress * (end - start) + start).toFixed(1));
+                setValue(progress * (end - start) + start);
                 if (progress < 1) {
                     window.requestAnimationFrame(step);
                 }
@@ -21,29 +20,32 @@ const GlobalCounter = () => {
             window.requestAnimationFrame(step);
         }, [end, start]);
 
-        return <span>{value}</span>;
+        return <span>{Number(value).toFixed(value % 1 === 0 ? 0 : 1)}</span>;
+
     };
 
     return (
-        <div className=" text-white lg:pt-14 md:pt-12 pt-[60px]  lg:pb-[175px] md:pb-12 pb-[60px] relative">
+        <div className="text-white lg:pt-14 pt-12  xl:pb-[87px] md:py-14 py-6 relative" id="solution">
             <div className="container mx-auto px-4 relative z-10">
                 <div className="gap-8 text-center flex max-md:flex-wrap items-center justify-center">
-                    {COUNTER_DATA.map(({ id, number, prefix = "", unit = "", description }, index) => (
+                    {COUNTER_DATA.map(({ number, prefix = "", unit = "", description }, index) => (
                         <div
-                            key={id}
+                            key={index}
                             className={`lg:w-[41%] w-full px-3 flex flex-col justify-center items-center max-lg:mb-6 ${index !== COUNTER_DATA.length - 1 ? "border-gradient" : ""
                                 }`}
                         >
-                            <div className="flex items-center justify-center lg:text-5xl text-3xl font-semibold">
-                                {prefix && <span className="lg:text-5xl text-3xl">{prefix}</span>}
+                            <div className="flex items-center justify-center lg:text-5xl text-2xl font-semibold">
+                                {prefix && <span className="lg:text-5xl lg:leading-custom-5xl text-2xl">{prefix}</span>}
                                 {index < 3 ? (
                                     <AnimateCount start={0} end={number} />
                                 ) : (
                                     <span>{number}</span>
                                 )}
-                                {unit && <span className="lg:text-5xl text-3xl">{unit}</span>}
+                                {unit && <span className="lg:text-5xl text-2xl">{unit}</span>}
                             </div>
-                            <p className="lg:text-base text-sm text-white font-normal mt-2 md:max-w-[187px]">{description}</p>
+                            <p className="sm:text-base text-sm text-white font-normal pt-2 md:max-w-[187px]">
+                                {description}
+                            </p>
                         </div>
                     ))}
                 </div>
@@ -52,4 +54,4 @@ const GlobalCounter = () => {
     );
 };
 
-export default GlobalCounter;
+export default BusinessCounter;
